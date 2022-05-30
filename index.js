1bb322ea9356dbeaab8256a754d8130ac61fd2c6
ValentinGTrapaga <gonzaleztrapagav@gmail.com>
2022-05-30 15:39:29 -0300

    Final commit - done in 1.5 hrs. Need some design course definitely

---
+++ index.js
@@ -0,0 +1,71 @@
+//Form
+const cityInput = document.getElementById('cityInput')
+const unitValue = document.querySelectorAll('input[name="unit"]')
+let unit = ""
+let symbol = ""
+let velocSymbol = ""
+
+
+//Display
+const app = document.getElementById('display')
+const cityName = document.getElementById('cityName')
+const weatherIcon = document.getElementById('weatherIcon')
+const weatherDesc = document.getElementById('weatherDesc')
+const weatherTemp = document.getElementById('weatherTemp')
+const windDesc = document.getElementById('windDesc')
+const humidity = document.getElementById('humidity')
+const pressure = document.getElementById('pressure')
+const windIcon = document.getElementById('windIcon')
+const humidityIcon = document.getElementById('humidityIcon')
+const h4 = document.querySelector('#title')
+
+const getWeather = event => {
+    event.preventDefault()
+    if (unitValue[0].checked) {
+        unit = "metric"
+        symbol = "°C"
+        velocSymbol = "m/s"
+    } else {
+        unit = "imperial"
+        symbol = "°F"
+        velocSymbol = "mph"
+    }
+    console.log(cityInput.value, unit)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value.toLowerCase()}&APPID=bd8cfe32865d2f6286924722b05aaf80&units=${unit}`)
+        .then(res => res.json())
+        .then(response => renderWeatherApp(response))
+        .catch(err => {
+            console.log(err)
+            renderErrorPage()
+        })
+}
+
+function capitalize(word) {
+    return word[0].toUpperCase() + word.slice(1)
+}
+
+const renderWeatherApp = data => {
+    const { name, main, weather, wind } = data
+    const country = data.sys.country
+    console.log(name, main, weather, wind, country);
+    cityName.textContent = `${name}, ${country}`
+    weatherIcon.src = `http://openweathermap.org/img/wn/${weather[0].icon}@4x.png`
+    weatherDesc.textContent = `${capitalize(weather[0].description)}`
+    weatherTemp.textContent = `${main["temp"]} ${symbol}`
+    windDesc.innerText = `Winds: ${wind["speed"]} ${velocSymbol}`
+    humidity.innerText = `Humidity: ${main["humidity"]} %`
+    pressure.innerText = `Pressure: ${main["pressure"]} hPa`
+}
+
+
+function renderErrorPage() {
+    cityName.textContent = "Unfortunately there's been an error in your request"
+    weatherIcon.src = ""
+    weatherDesc.textContent = ""
+    weatherTemp.textContent = ""
+    windDesc.textContent = ""
+    humidity.textContent = ""
+    pressure.textContent = ""
+    app.setAttribute = ("display", "none")
+}
+
